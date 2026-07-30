128d8fa66a1e433ec67b6a1f75390aa8fa301a4c
Joker20380 <jvov@mail.ru>
2026-07-30 12:51:08 +0300

    Remove obsolete guitar tuner endpoint

--- clearfield/intel/urls.py
+++ clearfield/intel/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from .views_guitar import guitar_tuner
 
 
 app_name = "intel"
 
 urlpatterns = [
-    path("guitar-tuner/", guitar_tuner, name="guitar_tuner"),
 ]
--- clearfield/intel/views.py
+++ clearfield/intel/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
 
 
-def guitar_tuner(request):
-    return render(request, "main/guitar_tuner.html")
